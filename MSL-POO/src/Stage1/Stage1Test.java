8bbff8c945152eb6d50a210eef2729c97a259080
socastro <65044886+socastro@users.noreply.github.com>
2020-05-13 22:30:47 -0400

    edit420

--- MSL-POO/src/Stage1/Stage1Test.java
+++ MSL-POO/src/Stage1/Stage1Test.java
@@ -7,14 +7,14 @@ import java.util.Locale;
 public class Stage1Test {
 
 	public static void main(String[] args) throws Exception {
-		Locale.setDefault(Locale.US);  // to read number in US format, like 1.5 (not like 1,5)
+	    Locale.setDefault(Locale.US);  // to read number in US format, like 1.5 (not like 1,5)
 	    Scanner in = new Scanner(new File(args[0]));
 	    float time = 0.0f;
 	    Joystick l_Joy = new Joystick();
 	    Joystick r_Joy = new Joystick();
 	    Operator operator = new Operator(in, l_Joy, r_Joy);
 	    while(operator.takeAction(time)) {
-	         System.out.println("\n" + time );
+	         System.out.println(time + "\n"  ); //tobecoded Aca deberiamos imprimir tiempo,   jih,   jiv,   jdh,  jdv
 	         time+=0.1;
 
 	    }
