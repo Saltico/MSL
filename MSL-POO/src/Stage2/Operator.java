10fd126c6345179d2b62c595341288ea9308f592
socastro <65044886+socastro@users.noreply.github.com>
2020-05-16 16:37:07 -0400

    done

--- MSL-POO/src/Stage2/Operator.java
+++ MSL-POO/src/Stage2/Operator.java
@@ -1,37 +1,37 @@
 import java.util.Scanner;
 
 public class Operator {
-	public Operator (Scanner in, Joystick l_Joy, Joystick r_Joy){
+	public Operator (Scanner in, SkyController skyController){  	//Operator recibe el archivo csv entregado
 	      inFile = in;
-	      l_Joystick = l_Joy;
-	      r_Joystick = r_Joy;
-	      inFile.nextLine(); // skip description line
-	      t = inFile.nextFloat();
+	      l_Joystick = skyController.getLeftStick();		//Se le asigna a left joystick el stick izquierdo del skyController
+	      r_Joystick = skyController.getRightStick();
+	      inFile.nextLine(); // skip description line	
+	      t = inFile.nextFloat();					//Con el nextFloat se lee el siguiente flotante
 
 	   }
 	   public boolean takeAction(float time){
 		 	float f;
-			if(time == 0){
+			if(time == 0){					//Toma la primera linea de datos para cuando el dron comience a funcionar
 				f = inFile.nextFloat();
-				l_Joystick.setHorPos(f);
+				l_Joystick.setHorPos(f);		//Luego con el set se fijan las variables horizontales y verticales 
 				f = inFile.nextFloat();
-	    	l_Joystick.setVerPos(f);
+	    			l_Joystick.setVerPos(f);
 				f = inFile.nextFloat();
-	    	r_Joystick.setHorPos(f);
+	    			r_Joystick.setHorPos(f);
 				f = inFile.nextFloat();
-	    	r_Joystick.setVerPos(f);
+	    			r_Joystick.setVerPos(f);
 				t = inFile.nextFloat();
 			}
-	   	else if(((Math.round(time*10))/10.0) >= t) {
+	   	else if(((Math.round(time*10))/10.0) >= t) {		//Se usa el redondeo para poder tener el tiempo con un digito de decimal
 				if(inFile.hasNextFloat()){
 					f = inFile.nextFloat();
 					l_Joystick.setHorPos(f);
 					f = inFile.nextFloat();
-	    		l_Joystick.setVerPos(f);
+	    				l_Joystick.setVerPos(f);
 					f = inFile.nextFloat();
-	    		r_Joystick.setHorPos(f);
+	    				r_Joystick.setHorPos(f);
 					f = inFile.nextFloat();
-	    		r_Joystick.setVerPos(f);
+	    				r_Joystick.setVerPos(f);
 					if(inFile.hasNextFloat()){
 						t=inFile.nextFloat();
 					}
